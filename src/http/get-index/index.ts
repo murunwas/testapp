218dcca9115ccfdec195c80b48183b352a4ad0d1
murunwas <cengyweb@gmail.com>
2020-07-05 10:22:28 +0200

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,4 +1,4 @@
-// TODO: modify the body object!
+// TODO: modify the body object!hhh
 let body = `
 <!doctype html>
 <html lang=en>
@@ -32,6 +32,7 @@ export async function handler (req: object) {
     body
   }
 }
+
 
 // Example responses
 
@@ -68,6 +69,7 @@ export async function handler (req: object) {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
     body: 'console.log("Hello world!")',
+
   }
 }
 */
